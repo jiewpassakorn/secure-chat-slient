5ce40934482ecf05cfd2a3272d6da265fe6a985e
Kwanchanok <quanchaa@gmail.com>
2023-05-24 00:36:28 +0700

    edit link

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173", // for react dev http://localhost:3000
+    origin: "https://646cf239e31b98000959de79--mellifluous-meerkat-c576c7.netlify.app/", // for react dev http://localhost:3000
   },
 });
 
