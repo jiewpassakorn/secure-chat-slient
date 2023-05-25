6af8414e76db264a2699381a55787cd3f2202a46
StanG12 <90758882+StanG12@users.noreply.github.com>
2023-05-25 14:30:18 +0700

    [Update] UpdateScript

--- backend/config/updateScript.js
+++ backend/config/updateScript.js
@@ -1,7 +1,44 @@
-const cron = require("node-cron");
-const axios = require("axios");
+//npm install -g railway
 
-// Function to delete old messages
+const { execSync } = require('child_process');
+const crypto = require('crypto');
+const axios = require('axios');
+const cron = require('node-cron');
+
+const generateSecretKey = (length) => {
+  const buffer = crypto.randomBytes(length);
+  return buffer.toString('hex');
+};
+
+const updateEnvVariableRailway = async (name, value) => {
+  try {
+    // Run the Railway CLI command to update the environment variable
+    const command = `railway env set ${name}=${value}`;
+    execSync(command, { stdio: 'inherit' });
+    console.log(`Environment variable ${name} updated successfully in Railway.`);
+  } catch (error) {
+    console.error(`Error updating environment variable ${name} in Railway: ${error.message}`);
+  }
+};
+
+const updateEnvVariableNetlify = async (siteId, apiAccessToken, name, value) => {
+  try {
+    // Send a PATCH request to the Netlify API to update the environment variable
+    const url = `https://api.netlify.com/api/v1/sites/${siteId}/env`;
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiAccessToken}`
+    };
+    const data = {
+      [name]: value
+    };
+    await axios.patch(url, data, { headers });
+    console.log(`Environment variable ${name} updated successfully in Netlify.`);
+  } catch (error) {
+    console.error(`Error updating environment variable ${name} in Netlify: ${error.message}`);
+  }
+};
+
 const deleteOldMessages = async () => {
   // Calculate the date 30 days ago
   const thirtyDaysAgo = new Date();
@@ -24,37 +61,25 @@ const deleteOldMessages = async () => {
   }
 };
 
-// Function to update Heroku config var
-const updateHerokuConfigVar = async () => {
-  const secretKey = generateSecretKey();
+const autoUpdateSecretKey = async () => {
+  // Generate a new secret key
+  const secretKey = generateSecretKey(32);
+  console.log('VITE_SECRET_KEY:', secretKey);
 
-  try {
-    const response = await axios.patch(
-      "https://api.heroku.com/apps/{your-app-name}/config-vars",
-      { SECRET_KEY: secretKey },
-      {
-        headers: {
-          Accept: "application/vnd.heroku+json; version=3",
-          Authorization: "Bearer {your-heroku-api-key}",
-        },
-      }
-    );
+  // Update the environment variable 'API_KEY' with the new secret key in Railway
+  await updateEnvVariableRailway('VITE_SECRET_KEY', secretKey);
 
-    console.log("Heroku config var updated successfully.");
-  } catch (error) {
-    console.error("Failed to update Heroku config var:", error.message);
-  }
+  // Update the environment variable 'API_KEY' with the new secret key in Netlify
+  const netlifySiteId = 'YOUR_NETLIFY_SITE_ID';
+  const netlifyApiAccessToken = 'YOUR_NETLIFY_API_ACCESS_TOKEN';
+  await updateEnvVariableNetlify(netlifySiteId, netlifyApiAccessToken, 'VITE_SECRET_KEY', secretKey);
 };
 
-// Schedule the code to run every 30 days (at 00:00)
-cron.schedule("0 0 */30 * *", async () => {
-  // Delete old messages
-  await deleteOldMessages();
-
-  // Update Heroku config var
-  await updateHerokuConfigVar();
+// Schedule the automatic update to run every 30 days
+cron.schedule('0 0 1 */30 *', () => {
+  autoUpdateSecretKey();
+  deleteOldMessages();
 });
 
-console.log(
-  "Scheduled job to delete old messages and update Heroku config var every 30 days."
-);
+// Run the initial update immediately
+autoUpdateSecretKey();
